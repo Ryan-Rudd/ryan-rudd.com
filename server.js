2e519dfcd322ce593a3c61abd54dd627f5e8d59a
Ryan Rudd <131509539+Ryan-Rudd@users.noreply.github.com>
2023-11-19 12:48:21 -0500

    new

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
 
 app.use(express.static('static'))
-
 app.get('/', (req, res)=>
 {
-    res.render('index')
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/about', (req, res)=>
-{
-    res.render('about')
-})
-app.get('/pricing', (req, res)=>
-{
-    res.render('pricing')
-})
-app.get('/contact', (req, res)=>
-{
-    res.render('contact')
-})
-
-app.listen(2000)
+app.listen(2000, ()=>{})
--- static/scripts/loader.js
+++
@@ -1,4 +0,0 @@
-    const preload = document.querySelector('.loading-container')
-    window.addEventListener('load', () => {
-        preload.remove()
-    })
